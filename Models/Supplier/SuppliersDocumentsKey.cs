ae3f7ad3a84e3caa9bbb2cb79a8d5e9df296c1a0
Antonio <atorre@2tinformatica.it>
2026-03-08 19:59:42 +0100

    Update

---
+++ Models/Supplier/SuppliersDocumentsKey.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace [Application Name].Models.Supplier
+{
+    public class SuppliersDocumentsKey
+    {
+        public enum documentType
+        {
+            CompanyPresentation = 1,
+            ProductsList = 2,
+            CompanyOrganization = 3,
+        }
+        public SuppliersDocumentsKey(int SupplierID, documentType DocumentType) { this.SupplierID = SupplierID; this.DocumentType = (int)DocumentType; }
+        [JsonPropertyName("DocumentType")]
+        public int DocumentType { get; set; }
+        [JsonPropertyName("SupplierID")]
+        public int SupplierID { get; set; }
+        public static string JsonKey(int SupplierID, documentType DocumentType)
+        {
+            return JsonConvert.SerializeObject(new SuppliersDocumentsKey(SupplierID, DocumentType));
+        }
+    }
+}
